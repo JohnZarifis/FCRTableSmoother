7efe285ada250f353da33a4cd35f46b4761efa28
John Zarifis <johnzarifis@gmail.com>
2017-04-25 11:42:55 +0300

    v 0.2
    local pandoc installation added

--- dist/runFCR_old.R
+++ dist/runFCR_old.R
@@ -1,7 +1,4 @@
 message('library paths:\n', paste('... ', .libPaths(), sep='', collapse='\n'))
 
-pandoc = file.path(getwd(), './dist/R-Portable/pandoc')
-
-Sys.setenv(PATH=paste(pandoc))
 
 rmarkdown::run('./dist/FCR/index.Rmd',shiny_args = list(port = 8241 , launch.browser=TRUE  ))
